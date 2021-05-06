228a6da8bd9f23ec600fc6adca51c863ebfce225
Beldineneema <79199587+Beldineneema@users.noreply.github.com>
2021-05-06 12:17:07 +0300

    Add files via upload

---
+++ assesment.kt
@@ -0,0 +1,20 @@
+ fun main(){
+
+var introduction= introduction("Hellen",19)
+ }
+ fun introduction(name:String,age:Int){
+     println("My name is $name and I am $age years old")
+
+ }
+fun machine(age:Int){
+    if (age<6){
+        println("Take a cup of milk")
+
+
+    }else if (age <6 && age< 15){
+        println("Here is fanta ")
+
+    }else{
+        println("here is a bottle of cocacola")
+    }
+}
